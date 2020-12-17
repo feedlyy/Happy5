1682af6759ab7d77be259a3d933ffd66bf0bd61f
Fadli <mnurfadd@gmail.com>
2020-12-18 01:39:19 +0700

    added database seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+# users.create(username: 'user1', password: bc)
+include BCrypt
+
+users = User
+
+users.destroy_all
+
+users.create!([{
+                 username: "user1",
+                 password: BCrypt::Password.create("pwduser1"),
+               },
+               {
+                 username: "user2",
+                 password: BCrypt::Password.create("pwduser2"),
+               },
+               {
+                 username: "user3",
+                 password: BCrypt::Password.create("pwduser3"),
+               },
+               {
+                 username: "user4",
+                 password: BCrypt::Password.create("pwduser4"),
+               }])
+
+p "Created #{users.count} users"
