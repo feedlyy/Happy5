ac5dd22fd6f2bdad36da4a3408ed2b63d882b78a
Fadli <mnurfadd@gmail.com>
2020-12-19 22:58:25 +0700

    added route for reply, read, rea-all

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   resources :users
 
   post '/auth/login', to: 'authentication#login'
-  get '/*a', to: 'application#not_found'
+  # get '/*a', to: 'application#not_found'
   post '/send', to: 'send_message#send_messages'
+  post '/reply', to: 'reply#reply'
+  get '/read', to: 'reply#read'
+  get '/list', to: 'list_chat#listchat'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
